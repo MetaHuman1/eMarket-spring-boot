c5d936c6ab28d08c246c24f86eb279907c13f8db
Shahab <breakkingsha@gmail.com>
2024-02-22 10:17:34 +0300

    Initial commit

---
+++ src/main/java/com/example/emarketspringboot/EMarketSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.emarketspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMarketSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EMarketSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/emarketspringboot/EMarketSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emarketspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMarketSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
